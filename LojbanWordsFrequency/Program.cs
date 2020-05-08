706d11be91d67047cb210902a05fdf277c4e48c5
skytomo <tomo1kawa.fullerene@gmail.com>
2020-05-08 19:39:44 +0900

    Add readme

--- LojbanWordsFrequency/Program.cs
+++ LojbanWordsFrequency/Program.cs
@@ -9,7 +9,7 @@ namespace MeltywordsProject
         {
             string text;
             System.Console.WriteLine("Loading start...");
-            using (var reader = new StreamReader(@"../../../output.txt"))
+            using (var reader = new StreamReader(@"output.txt"))
             {
                 text = reader.ReadToEnd();
             }
@@ -18,14 +18,14 @@ namespace MeltywordsProject
             int all = list.Count();
             int sum = 0;
             System.Console.WriteLine("The list complete. (" + all + " words)");
-            File.WriteAllText(@"../../../freq.tsv", "");
+            File.WriteAllText(@"freq.tsv", "");
             while (list.Count > 0)
             {
                 var s = list[0];
                 int cnt = list.Count(i => i == s);
                 sum += cnt;
                 System.Console.WriteLine(string.Format("{0}\t{1, 10}\t{2:0.000}%", s, cnt, ((double)sum / all * 100)));
-                File.AppendAllText(@"../../../freq.tsv", s.Trim() + "\t" + cnt + "\n");
+                File.AppendAllText(@"freq.tsv", s.Trim() + "\t" + cnt + "\n");
                 list.RemoveAll(i => i == s);
             }
         }
---
+++ LojbanWordsFrequency/README.md
@@ -0,0 +1,25 @@
+# LojbanWordsFrequency
+
+## How do I?
+
+1. `git clone https://github.com/skytomo221/LojbanWordsFrequency.git`
+2. Go http://corpus.lojban.org/ and download [corpus.txt.bz2](http://corpus.lojban.org/corpus/corpus.txt.bz2) to `./LojbanWordsFrequency/LojbanWordsFrequency`
+3. Unzip corpus.txt.bz2.
+4. `cd ./LojbanWordsFrequency/LojbanWordsFrequency`
+5. `py ./lojban-corpus-formatter.py`
+6. `dotnet run`
+
+### Visual Studio Version
+
+1. `git clone https://github.com/skytomo221/LojbanWordsFrequency.git`
+2. Build with release.
+3. Go http://corpus.lojban.org/ and download [corpus.txt.bz2](http://corpus.lojban.org/corpus/corpus.txt.bz2) to `./LojbanWordsFrequency/LojbanWordsFrequency/bin/Release/netcoreapp3.1/`
+4. Unzip corpus.txt.bz2.
+5. `cd ./LojbanWordsFrequency/LojbanWordsFrequency/bin/Release/netcoreapp3.1/`
+6. `py ./lojban-corpus-formatter.py`
+7. Open the project with Visual Studio.
+8. Press <kbd>Ctrl</kbd>+<kbd>F5</kbd> (Run without debugging)
+
+## Example
+
+![](https://pbs.twimg.com/media/EXfIMC2U4AAJOqX?format=png&name=small)
